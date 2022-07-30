73196b3cd0b347bda69b2e882f5c4868b893b450
Aashick John <aashickjohn2792@gmail.com>
2022-07-30 18:32:17 +0530

    First commit

---
+++ simp.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.append('C:\\Users\\Aashick\\OneDrive\\Desktop\\Learnings\\simple-flask-ci-cd\\src')
+print(sys.path)
+
+# print('hi')
+# # for k, v in dict(os.environ).items():
+# #     if k == 'PATH':
+# #         # print(f'PATH : {v.split(';')}')
+# #         for pat in v.split(';'):
+# #             if 'Python' in pat:
+# #                 print(f'Path : {pat}')
+# print(os.environ.get('PATH'))
+
+
+# str1 = str("C:\Program Files\Python3\Scripts\;C:\Program Files\Python3\;")
+
+# print('python' in str1)
